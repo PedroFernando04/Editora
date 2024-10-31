c53081764de47e147b9b4047f51dac0340f25f0d
Pedro Fernando <106192736+PedroFernando04@users.noreply.github.com>
2024-10-31 14:45:09 -0300

    Create script.py

---
+++ conexao_python/script.py
@@ -0,0 +1,10 @@
+from editora.criar_conexao_editora import conexao
+from editora.insert_editora import inserir_autor, inserir_editora, inserir_genero, inserir_livro
+
+inserir_autor("Michael Dante DiMartino", "19740718", "M")
+
+inserir_editora("Planeta")
+
+inserir_genero("Comédia")
+
+inserir_livro("A ascensão de Kyoshi: O passado da poderosa Avatar do Reino da Terra", "8", "8", '20190716', "1")
