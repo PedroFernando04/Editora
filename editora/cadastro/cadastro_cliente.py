ddb7027a35258d6342f636e83412cae4f8401377
Pedro Fernando <106192736+PedroFernando04@users.noreply.github.com>
2024-11-01 15:33:02 -0300

    Update cadastro_cliente.py

--- editora/cadastro/cadastro_cliente.py
+++ editora/cadastro/cadastro_cliente.py
@@ -1,12 +1,18 @@
 from insert_editora import inserir_cliente
 from verificar_email import verificar_email
+from senha_valida import senha_valida
+from email_valido import email_valido
 
 def cadastrar_cliente():
     print("Vamos realizar o seu cadastro!")
     print("Por favor, preencha as informações solicitadas:\n")
-    nome = input("Nome: ")
-    ler_email = input("E-mail: ")
-    email = verificar_email(ler_email)
     
+    nome = input("Nome: ")
+    
+    email = verificar_email(email_valido())
+    
+    senha = senha_valida()
 
-    #inserir_cliente(nome, email, senha, genero, pais, data)
+    genero = 1
+
+    inserir_cliente(nome, email, senha, genero, pais, data)
