90eee8044bdeca3ce022d62ebededfd3eda9000f
Pedro Fernando <106192736+PedroFernando04@users.noreply.github.com>
2024-11-01 17:51:23 -0300

    Update INSERT.sql

--- tabelas/INSERT.sql
+++ tabelas/INSERT.sql
@@ -195,6 +195,11 @@ INSERT INTO paises (nome_pais) VALUES
 ('Zâmbia'),
 ('Zimbábue');
 
+INSERT INTO status(nome_status)
+VALUES
+  ('Lido'),
+  ('Em progresso'),
+  ('Lista de desejos');
 
 INSERT INTO autores(nome_autor, data_nascimento, genero_autor, pais_autor)
 VALUES
@@ -236,15 +241,15 @@ VALUES
 
 INSERT INTO livros(nome_livro, id_autor_livro, id_editora_livro, data_de_lancamento, id_genero_livro, status_livro, ano_lido, nota, resenha, id_cliente_livro)
 VALUES
-    ('O Espadachim de Carvão', 1, 1, '20130101', 1, 'Lido', 2024, 9, null, 2),
-    ('Caixa de Pássaros', 4, 3, '20150121', 2, 'Lido', 2024, 9, null, 2),
-    ('O Curioso Caso de Benjamin Button', 5, 6, '19220527', 3, 'Lido', 2024, 6, null, 2),
-    ('O Espadachim de Carvão e a voz do Guardião Cego', 1, 1, '20211020', 1, 'Lido', 2024, 9, null, 2),
-    ('Jurassick Park', 6, 4, '19901120', 4, 'Em leitura', 2024, null, null, 2),
-    ('Morte no Nilo', 2, 2, '19371101', 6, 'Lido', 2024, 10, null, 2),
-    ('O Santo Guerreiro: Roma Invicta', 7, 7, '20201207', 5, 'Na fila', 2024, null, null, 2),
-    ('O Leão, a Feiticeira e o Guarda-Roupa', 3, 2, '19501016', 7, 'Lido', 2024, 7, null, 2),
-    ('Espadachim de Carvão e as pontes de Puzur', 1, 1, '20150910', 1, 'Lido', 2024, 9, null, 2),
-    ('A ascensão de Kyoshi: O passado da poderosa Avatar do Reino da Terra', 8, 8, '20190716', 1, 'Em leitura', 2024, null, null, 2);
+    ('O Espadachim de Carvão', 1, 1, '20130101', 1, 1, 2024, 9, null, 2),
+    ('Caixa de Pássaros', 4, 3, '20150121', 2, 1, 2024, 9, null, 2),
+    ('O Curioso Caso de Benjamin Button', 5, 6, '19220527', 3, 1, 2024, 6, null, 2),
+    ('O Espadachim de Carvão e a voz do Guardião Cego', 1, 1, '20211020', 1, 1, 2024, 9, null, 2),
+    ('Jurassick Park', 6, 4, '19901120', 4, 2, 2024, null, null, 2),
+    ('Morte no Nilo', 2, 2, '19371101', 6, 1, 2024, 10, null, 2),
+    ('O Santo Guerreiro: Roma Invicta', 7, 7, '20201207', 5, 3, 2024, null, null, 2),
+    ('O Leão, a Feiticeira e o Guarda-Roupa', 3, 2, '19501016', 7, 1, 2024, 7, null, 2),
+    ('Espadachim de Carvão e as pontes de Puzur', 1, 1, '20150910', 1, 1, 2024, 9, null, 2),
+    ('A ascensão de Kyoshi: O passado da poderosa Avatar do Reino da Terra', 8, 8, '20190716', 1, 2, 2024, null, null, 2);
   
   
