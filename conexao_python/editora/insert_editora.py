db5c70aaa1d09cc8d3fb8c96c047e93ee81599f8
Pedro Fernando <106192736+PedroFernando04@users.noreply.github.com>
2024-11-01 11:06:11 -0300

    Update insert_editora.py

--- conexao_python/editora/insert_editora.py
+++ conexao_python/editora/insert_editora.py
@@ -1,11 +1,11 @@
 from editora.criar_conexao_editora import conexao
 
-def inserir_autor(nome, data, genero):
+def inserir_autor(nome, data, genero, pais):
     conn = conexao()
     try:
         cursor = conn.cursor()
-        query = "INSERT INTO autores(nome_autor, data_nascimento, genero_autor) VALUES (%s, %s, %s);"
-        cursor.execute(query, (nome, data, genero))
+        query = "INSERT INTO autores(nome_autor, data_nascimento, genero_autor) VALUES (%s, %s, %s, %s);"
+        cursor.execute(query, (nome, data, genero, pais))
         conn.commit()
         print("Autor cadastrado com sucesso!")
     except Exception as e:
@@ -42,12 +42,26 @@ def inserir_genero(nome):
         cursor.close()
         conn.close()
 
-def inserir_livro(nome, id_autor, id_editora, data, id_genero):
+def inserir_cliente(nome, email, senha, genero, pais, data):
     conn = conexao()
     try:
         cursor = conn.cursor()
-        query = "INSERT INTO livros(nome_livro, id_autor_livro, id_editora_livro, data_de_lancamento, id_genero_livro) VALUES (%s, %s, %s, %s, %s);"
-        cursor.execute(query, (nome, id_autor, id_editora, data, id_genero))
+        query = "INSERT INTO clientes(nome_cliente, email_cliente, senha_cliente, genero_cliente, pais_cliente, data_nascimento_cliente) VALUES(%s, %s, %s, %s, %s, %s)"
+        cursor.execute(query, (nome, email, senha, genero, pais, data))
+        conn.commit()
+        print("Usuario cadastrado com sucesso!")
+    except Exception as e:
+        print(f"ERRO CLIENTE: {e}")
+    finally:
+        cursor.close()
+        conn.close()
+
+def inserir_livro(nome, id_autor, id_editora, data, id_genero, status, ano, nota, resenha, id_cliente):
+    conn = conexao()
+    try:
+        cursor = conn.cursor()
+        query = "INSERT INTO livros(nome_livro, id_autor_livro, id_editora_livro, data_de_lancamento, id_genero_livro) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s);"
+        cursor.execute(query, (nome, id_autor, id_editora, data, id_genero, status, ano, nota, resenha, id_cliente))
         conn.commit()
         print("Livro cadastrado com sucesso!")
     except Exception as e:
