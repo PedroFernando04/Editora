0c2219c5d5d56f4eaa6dda1e876f452b9b982c5a
Pedro Fernando <106192736+PedroFernando04@users.noreply.github.com>
2024-11-01 14:14:51 -0300

    Create verificar_email.py

---
+++ conexao_python/editora/verificar_email.py
@@ -0,0 +1,23 @@
+from criar_conexao_editora import conexao
+
+def verificar_email(email):
+    conn = conexao()
+    try:
+        cursor = conn.cursor()
+        query = "SELECT email_cliente FROM clientes WHERE email_cliente = %s"
+        cursor.execute(query, (email,))
+        conn.commit()
+        row = cursor.fetchall()
+        print("email verificado!")
+        if row:
+            print('E-mail já cadastrado!')
+            email = input("Por favor, insira outro e-mail: ")
+            verificar_email(email)
+        else:
+            print("E-mail válido!")
+    except Exception as e:
+        print(f"ERRO EMAIL: {e}")
+    finally:
+        cursor.close()
+        conn.close()
+    return row
