3d41a80a9130bd81911a3c900547dcefe43f4adb
Pedro Fernando <106192736+PedroFernando04@users.noreply.github.com>
2024-11-01 14:33:22 -0300

    Atualizar o cadastro_cliente.py

--- conexao_python/editora/cadastro_cliente.py
+++ conexao_python/editora/cadastro_cliente.py
@@ -5,8 +5,8 @@ def cadastrar_cliente():
     print("Vamos realizar o seu cadastro!")
     print("Por favor, preencha as informações solicitadas:\n")
     nome = input("Nome: ")
-    email = input("E-mail: ")
-    verificar_email(email)
+    ler_email = input("E-mail: ")
+    email = verificar_email(ler_email)
     
 
     #inserir_cliente(nome, email, senha, genero, pais, data)
