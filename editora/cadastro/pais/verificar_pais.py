40bf66a4088e5eea220e69a3db7bfd957946f3fb
Pedro Fernando <106192736+PedroFernando04@users.noreply.github.com>
2024-11-01 17:18:09 -0300

    Update verificar_pais.py

--- editora/cadastro/pais/verificar_pais.py
+++ editora/cadastro/pais/verificar_pais.py
@@ -1,1 +1,24 @@
+from criar_conexao_editora import conexao
 
+def verificar_pais(pais):
+    conn = conexao()
+    while True:
+        try:
+            cursor = conn.cursor()
+            query = "SELECT email_cliente FROM clientes WHERE email_cliente = %s"
+            cursor.execute(query, (pais,))
+            conn.commit()
+            row = cursor.fetchall()
+            print("email verificado!")
+            if row:
+                print('País presente no banco')
+                return pais
+            else:
+                print("País desconhecido!")
+                print("Tente novamente!")
+        except Exception as e:
+            print(f"ERRO PAIS: {e}")
+        finally:
+            cursor.close()
+            conn.close()
+    
