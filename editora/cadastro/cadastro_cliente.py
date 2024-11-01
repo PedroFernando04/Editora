037ac259ad7a3ce03d57291ac9a31758a44d146d
Pedro Fernando <106192736+PedroFernando04@users.noreply.github.com>
2024-11-01 15:48:15 -0300

    Update cadastro_cliente.py

--- editora/cadastro/cadastro_cliente.py
+++ editora/cadastro/cadastro_cliente.py
@@ -2,6 +2,7 @@ from insert_editora import inserir_cliente
 from verificar_email import verificar_email
 from senha_valida import senha_valida
 from email_valido import email_valido
+from genero_valido import genero_valido
 
 def cadastrar_cliente():
     print("Vamos realizar o seu cadastro!")
@@ -13,6 +14,7 @@ def cadastrar_cliente():
     
     senha = senha_valida()
 
-    genero = 1
+    genero = genero_valido()
 
+    pais = 1
     inserir_cliente(nome, email, senha, genero, pais, data)
