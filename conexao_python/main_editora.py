7b75fbd57d57935854354f9ef398b1a408d1447a
Pedro Fernando <106192736+PedroFernando04@users.noreply.github.com>
2024-11-01 14:15:27 -0300

    Update and rename script.py to main_editora.py

---
+++ conexao_python/main_editora.py
@@ -0,0 +1,15 @@
+from editora.insert_editora import inserir_autor, inserir_editora, inserir_genero, inserir_livro, inserir_cliente
+from editora.cadastro_cliente import cadastrar_cliente
+
+nome_editora = 'Edicria'
+print(f"Bem vido a {nome_editora}!")
+print("Aqui registramos os seus livros para sua melhor visualização\n")
+while True:
+    print("Informe o que deseja fazer:\n")
+    print("1 - Casdastro")
+    print("2 - Login")
+    print("3 - Sair\n")
+    opc = int(input())
+
+    if opc == 1:
+        cadastro_cliente = cadastrar_cliente()
