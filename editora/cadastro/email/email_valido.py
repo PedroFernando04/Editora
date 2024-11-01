8da3d4b5b6accebd753163c5568092b958117587
Pedro Fernando <106192736+PedroFernando04@users.noreply.github.com>
2024-11-01 15:25:39 -0300

    Update email_valido.py

--- editora/cadastro/email/email_valido.py
+++ editora/cadastro/email/email_valido.py
@@ -1,1 +1,13 @@
+def email_valido():
+    while True:
+        email = input("E-mail: ")
+        if '@' in email and '.' in email:
+            print("E-mail válido!")
+            return email
+            
+        else:
+            print("\nE-mail inválido!")
+            print("Um e-mail válido deve conter um \'@\' e um \'.\'")
+            print("Tente novamente\n")
 
+print(email_valido())
