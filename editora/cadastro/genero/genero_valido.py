b0f392c07b017328279504c7d5741d7fa320567d
Pedro Fernando <106192736+PedroFernando04@users.noreply.github.com>
2024-11-01 15:48:31 -0300

    Update genero_valido.py

--- editora/cadastro/genero/genero_valido.py
+++ editora/cadastro/genero/genero_valido.py
@@ -1,1 +1,10 @@
-
+def genero_valido():
+    while True:
+        print("M - Masculino\nF - Feminino\nI - Indefinido")
+        genero = input("\nGenero: ").upper()
+        if genero not in ['M', 'F', 'I']:
+            print("\nValor inválido!")
+            print("Informe seu gênero seguindo a tabela\n")
+        else:
+            print("\nGenero cadastrado com sucesso!")
+            return genero
