dff555fdc755140e47cd1bc867b68ab4fc126c4e
Pedro Fernando <106192736+PedroFernando04@users.noreply.github.com>
2024-11-02 20:51:33 -0300

    Delete editora directory

--- editora/cadastro/senha/senha_valida.py
+++
@@ -1,9 +0,0 @@
-def senha_valida():
-    senha1 = input("Senha: ")
-    senha2 = input("Insira a senha novamente: ")
-
-    if senha1 == senha2:
-        return senha1
-    else:
-        print("Senhas não correspondentes\nTente novamente!")
-        senha_valida()
