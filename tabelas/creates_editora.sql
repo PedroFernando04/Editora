e3d033475b8f7f51527f52680853509a2b9d6de2
Pedro Fernando <106192736+PedroFernando04@users.noreply.github.com>
2024-11-18 09:15:51 -0300

    Update creates_editora.sql

--- tabelas/creates_editora.sql
+++ tabelas/creates_editora.sql
@@ -11,7 +11,7 @@ CREATE TABLE status(
 CREATE TABLE autores(
   id_autor serial primary key,
   nome_autor varchar(500) not null,
-  data_nascimento date not null,
+  data_nascimento_autor date not null,
   genero_autor char(1),
   pais_autor int references paises(id_pais)
 );
@@ -32,7 +32,8 @@ CREATE TABLE clientes(
   email_cliente varchar(500),
   senha_cliente varchar(500),
   genero_cliente char(1),
-  pais_cliente int references paises(id_pais)
+  pais_cliente int references paises(id_pais),
+  data_nascimento_cliente date not null
 );
 
 CREATE TABLE livros(
@@ -40,12 +41,12 @@ CREATE TABLE livros(
   nome_livro varchar(500) not null unique,
   id_autor_livro int references autores(id_autor),
   id_editora_livro int references editoras(id_editora),
-  data_de_lancamento date,
+  data_de_lancamento_livro date,
   id_genero_livro int references generos(id_genero),
   status_livro int references status(id_status),
-  ano_lido int,
-  nota int,
-  resenha varchar(1000),
+  ano_lido_livro int,
+  nota_livro int,
+  resenha_livro varchar(1000),
   id_cliente_livro int references clientes(id_cliente)
 );
 
