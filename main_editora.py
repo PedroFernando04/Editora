40d2bf34dd73e2cfa2842186be8edda6c72fa4e5
Pedro Fernando <106192736+PedroFernando04@users.noreply.github.com>
2024-11-18 17:25:32 -0300

    Update main_editora.py

--- main_editora.py
+++ main_editora.py
@@ -1,5 +1,7 @@
 from editora.inserts.insert_editora import inserir_autor, inserir_editora, inserir_genero, inserir_livro, inserir_cliente
 from editora.cadastro.cadastro_cliente import cadastrar_cliente
+from editora.login.login_ciente import logar_cliente
+import os
 
 nome_editora = 'Edicria'
 print(f"Bem vido a {nome_editora}!")
@@ -9,11 +11,31 @@ while True:
     print("1 - Casdastro")
     print("2 - Login")
     print("3 - Sair\n")
-    opc = int(input())
+    opc = (input())
 
-    if opc == 1:
+    if opc == '1':
+        os.system('cls' or 'clear')
         #função retorna toso os campos do cadastro
         cadastro_cliente = cadastrar_cliente()
-    elif opc == 2:
+    elif opc == '2':
+        os.system('cls' or 'clear')
         #função booleana
         login_cliente = logar_cliente()
+
+        if login_cliente:
+            os.system('cls' or 'clear')
+            print("Login realizado com sucesso!")
+            input()
+            #entra na editora
+        else:
+            os.system('cls' or 'clear')
+            print("Login ou senha incorreta!")
+            #volta pro loop do menu
+    elif opc == '3':
+        os.system('cls' or 'clear')
+        print("Finalizando...")
+        break
+    else:
+        os.system('cls' or 'clear')
+        print("Valor inválido!")
+        print("Tente novamente")
