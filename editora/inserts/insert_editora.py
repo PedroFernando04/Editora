392fc7cfc7a74a218e9a3160e767d99ddc359911
Pedro Fernando <106192736+PedroFernando04@users.noreply.github.com>
2024-11-18 17:24:20 -0300

    Update insert_editora.py

--- editora/inserts/insert_editora.py
+++ editora/inserts/insert_editora.py
@@ -4,7 +4,7 @@ def inserir_autor(nome, data, genero, pais):
     conn = conexao()
     try:
         cursor = conn.cursor()
-        query = "INSERT INTO autores(nome_autor, data_nascimento, genero_autor, pais_autor) VALUES (%s, %s, %s, %s);"
+        query = "INSERT INTO editora.autores(nome_autor, data_nascimento, genero_autor, pais_autor) VALUES (%s, %s, %s, %s);"
         cursor.execute(query, (nome, data, genero, pais))
         conn.commit()
         print("Autor cadastrado com sucesso!")
@@ -18,7 +18,7 @@ def inserir_editora(nome):
     conn = conexao()
     try:
         cursor= conn.cursor()
-        query = "INSERT INTO editoras(nome_editora) VALUES (%s);"
+        query = "INSERT INTO editora.editoras(nome_editora) VALUES (%s);"
         cursor.execute(query, (nome,))
         conn.commit()
         print(f"Editora cadastrada com sucesso!")
@@ -32,7 +32,7 @@ def inserir_genero(nome):
     conn = conexao()
     try:
         cursor = conn.cursor()
-        query = "INSERT INTO generos(nome_genero) VALUES (%s);"
+        query = "INSERT INTO editora.generos(nome_genero) VALUES (%s);"
         cursor.execute(query, (nome,))
         conn.commit()
         print("Genero cadastrado com sucesso!")
@@ -46,7 +46,7 @@ def inserir_cliente(nome, email, senha, genero, pais, data):
     conn = conexao()
     try:
         cursor = conn.cursor()
-        query = "INSERT INTO clientes(nome_cliente, email_cliente, senha_cliente, genero_cliente, pais_cliente, data_nascimento_cliente) VALUES(%s, %s, %s, %s, %s, %s)"
+        query = "INSERT INTO editora.clientes(nome_cliente, email_cliente, senha_cliente, genero_cliente, pais_cliente, data_nascimento_cliente) VALUES(%s, %s, %s, %s, %s, %s)"
         cursor.execute(query, (nome, email, senha, genero, pais, data))
         conn.commit()
         print("Usuario cadastrado com sucesso!")
@@ -60,7 +60,7 @@ def inserir_livro(nome, id_autor, id_editora, data, id_genero, status, ano, nota
     conn = conexao()
     try:
         cursor = conn.cursor()
-        query = "INSERT INTO livros(nome_livro, id_autor_livro, id_editora_livro, data_de_lancamento, id_genero_livro) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s);"
+        query = "INSERT INTO editora.livros(nome_livro, id_autor_livro, id_editora_livro, data_de_lancamento, id_genero_livro) VALUES (%s, %s, %s, %s, %s);"
         cursor.execute(query, (nome, id_autor, id_editora, data, id_genero, status, ano, nota, resenha, id_cliente))
         conn.commit()
         print("Livro cadastrado com sucesso!")
@@ -69,3 +69,4 @@ def inserir_livro(nome, id_autor, id_editora, data, id_genero, status, ano, nota
     finally:
         cursor.close()
         conn.close()
+
