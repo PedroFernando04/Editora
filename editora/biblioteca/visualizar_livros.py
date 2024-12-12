be103b32fd432ede212c459a39371a314a8cae40
Pedro Fernando <106192736+PedroFernando04@users.noreply.github.com>
2024-12-12 11:25:15 -0300

    Update visualizar_livros.py

--- editora/biblioteca/visualizar_livros.py
+++ editora/biblioteca/visualizar_livros.py
@@ -1,4 +1,3 @@
-
 def visualizar_livros(conn, id_cliente):
     
     cursor = conn.cursor()
@@ -6,6 +5,7 @@ def visualizar_livros(conn, id_cliente):
                 SELECT nome_livro, nome_autor, nome_editora, 
                 data_de_lancamento_livro, nome_genero, nome_status, 
                 ano_lido_livro, nota_livro, resenha_livro
+                
                 FROM editora.livros l 
                 JOIN editora.autores a ON l.id_autor_livro = a.id_autor
                 JOIN editora.editoras e ON l.id_editora_livro = e.id_editora
@@ -17,29 +17,33 @@ def visualizar_livros(conn, id_cliente):
     cursor.execute(query, (id_cliente,))
     livros = cursor.fetchall()
 
-    for livro in livros:
-        info_livro = (
-            livro[0],
-            livro[1],
-            livro[2],
-            livro[3],
-            livro[4],
-            livro[5],
-            livro[6],
-            livro[7],
-            livro[8]
-        )
+    if livros:
+        for livro in livros:
+            info_livro = (
+                livro[0],
+                livro[1],
+                livro[2],
+                livro[3],
+                livro[4],
+                livro[5],
+                livro[6],
+                livro[7],
+                livro[8]
+            )
 
-        (nome, autor, editora, data_lancamento, genero,  status, ano_lido, nota, resenha) = info_livro
-        
-        print("-"*120)
-        print(f"Nome: {nome}")
-        print(f"Autor(a): {autor}")
-        print(f"Editora: {editora}")
-        print(f"Data de Lançamento: {data_lancamento}")
-        print(f"Gênero: {genero}")
-        print(f"Status: {status}")
-        print(f"Ano lido: {ano_lido}")
-        print(f"Nota: {nota}")
-        print(f"Resenha: {resenha}")
-        print("-"*120)
+            (nome, autor, editora, data_lancamento, genero,  status, ano_lido, nota, resenha) = info_livro
+            
+            print("-"*120)
+            print(f"Nome: {nome}")
+            print(f"Autor(a): {autor}")
+            print(f"Editora: {editora}")
+            print(f"Data de Lançamento: {data_lancamento}")
+            print(f"Gênero: {genero}")
+            print(f"Status: {status}")
+            print(f"Ano lido: {ano_lido}")
+            print(f"Nota: {nota}")
+            print(f"Resenha: {resenha}")
+            print("-"*120)
+
+    else:
+        print("Nenhum livro cadastrado!")
