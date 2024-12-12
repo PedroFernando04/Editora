c176c0d26ce06a3285d815d24ee7c39586308c4d
Pedro Fernando <106192736+PedroFernando04@users.noreply.github.com>
2024-12-12 11:23:33 -0300

    Update main_editora.py

--- main_editora.py
+++ main_editora.py
@@ -1,41 +1,47 @@
 from editora.inserts.insert_editora import inserir_autor, inserir_editora, inserir_genero, inserir_livro, inserir_cliente
 from editora.cadastro.cadastro_cliente import cadastrar_cliente
-from editora.login.login_ciente import logar_cliente
-import os
+from editora.login.login_cliente import logar_cliente
+from editora.conexao.criar_conexao_editora import conexao
+from editora.biblioteca.menu_editora import menu_editora
 
+from editora.defs.defs_basicas import *
+
+conn = conexao()
+
+clear()
 nome_editora = 'Edicria'
 print(f"Bem vido a {nome_editora}!")
 print("Aqui registramos os seus livros para sua melhor visualização\n")
 while True:
     print("Informe o que deseja fazer:\n")
-    print("1 - Casdastro")
+    print("1 - Cadastro")
     print("2 - Login")
     print("3 - Sair\n")
     opc = (input())
+    clear()
 
     if opc == '1':
-        os.system('cls' or 'clear')
         #função retorna toso os campos do cadastro
-        cadastro_cliente = cadastrar_cliente()
+        cadastro_cliente = cadastrar_cliente(conn)
     elif opc == '2':
-        os.system('cls' or 'clear')
+        
         #função booleana
-        login_cliente = logar_cliente()
+        id_cliente = logar_cliente(conn)
 
-        if login_cliente:
-            os.system('cls' or 'clear')
-            print("Login realizado com sucesso!")
-            input()
+        if id_cliente:
+            delay()
             #entra na editora
+            menu_editora(conn, id_cliente)
         else:
-            os.system('cls' or 'clear')
-            print("Login ou senha incorreta!")
+            delay()
             #volta pro loop do menu
+
     elif opc == '3':
-        os.system('cls' or 'clear')
         print("Finalizando...")
         break
+
     else:
-        os.system('cls' or 'clear')
         print("Valor inválido!")
         print("Tente novamente")
+
+conn.close()
