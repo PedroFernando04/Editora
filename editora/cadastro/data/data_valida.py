f7afedfd383c7651c4b9777b7e262c797ee7bbb2
Pedro Fernando <106192736+PedroFernando04@users.noreply.github.com>
2024-12-12 11:57:34 -0300

    Update data_valida.py

--- editora/cadastro/data/data_valida.py
+++ editora/cadastro/data/data_valida.py
@@ -1,11 +1,11 @@
-import os
+from editora.defs.defs_basicas import *
 
 def validacao_data(dia, mes, num, tipo, ano_atual):
     while True:
         match tipo:
                 case 'dia':
                     if num < 1 or num > 31:
-                        os.system('cls' or 'clear')
+                        clear()
                         print("Valor inválido!")
                         print(f"O {tipo} deve ser um número entre 1 e 31\n")
                         num = int(input("Dia: "))
@@ -13,7 +13,7 @@ def validacao_data(dia, mes, num, tipo, ano_atual):
                         return num
                 case 'mes':
                     if num < 1 or num > 12:
-                        os.system('cls' or 'clear')
+                        clear()
                         print("Valor inválido!")
                         print(f"O {tipo} deve ser um número entre 1 e 12\n")
                         print(f"Dia: {dia}")
@@ -22,7 +22,7 @@ def validacao_data(dia, mes, num, tipo, ano_atual):
                         return num
                 case _:
                     if num > ano_atual or num < 0:
-                        os.system('cls' or 'clear')
+                        clear()
                         print("Valor inválido!")
                         print(f"O {tipo} deve ser um número entre 1 e {ano_atual}\n")
                         print(f"Dia: {dia}")
@@ -46,10 +46,10 @@ def try_dia():
         try:
             dia = int(input("Dia: "))
             dia_valido = validacao_data(0, 0, dia, 'dia', 2024)
-            os.system('cls' or 'clear')
+            clear()
             return str(dia_valido)
         except Exception as e:
-            os.system('cls' or 'clear')
+            clear()
             print("Formato inválido!")
             print("Informe dia válido!\n")
 def try_mes(dia):
@@ -60,14 +60,14 @@ def try_mes(dia):
             if dia > 29 and mes == 2:
                 raise ValueError ('erro_fevereiro')
             mes_valido = validacao_data(dia, 0, mes, 'mes', 2024)
-            os.system('cls' or 'clear')
+            clear()
             return str(mes_valido)
         except Exception as e:
-            os.system('cls' or 'clear')
+            clear()
             print("Formato inválido!")
             print("Informe um mês válido!\n")
         except 'erro_fevereiro':
-            os.system('cls' or 'clear')
+            clear()
             print("Formato inválido!")
             print(f"Data: {dia}/{mes} Não existe!\n")
             print("Informe um mês válido!\n")
@@ -80,11 +80,11 @@ def try_ano(dia, mes):
             print(f"Mês: {mes}")
             ano = int(input("Ano: "))
             ano_valido = validacao_data(dia, mes, ano, 'ano', 2024)
-            os.system('cls' or 'clear')
+            clear()
             return str(ano_valido)
         
         except Exception as e:
-            os.system('cls' or 'clear')
+            clear()
             print(e)
             print("Formato inválido!")
             print("Informe um ano válido!\n")
@@ -102,9 +102,8 @@ def data_valida():
 
     ano_valido = try_ano(dia_valido, mes_valido)
 
-    os.system('cls' or 'clear')
+    clear()
     data = f"{ano_valido}{mes_valido}{dia_valido}"
 
     print(f"Data de nascimento: {dia_valido}/{mes_valido}/{ano_valido}")
     return data
-                
