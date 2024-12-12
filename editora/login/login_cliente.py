ca97b4b881258056bb8d6143c65e1b2ccc82a0dc
Pedro Fernando <106192736+PedroFernando04@users.noreply.github.com>
2024-12-12 14:12:30 -0300

    Update login_cliente.py

--- editora/login/login_cliente.py
+++ editora/login/login_cliente.py
@@ -1,26 +1,19 @@
-
-
 def logar_cliente(conn):
     email = input("Login: ")
     senha = input("Senha: ")
 
     try:
         cursor = conn.cursor()
-        query = "SELECT * FROM editora.clientes WHERE email_cliente = %s AND senha_cliente = %s"
+        query = "SELECT id_cliente FROM editora.clientes WHERE email_cliente = %s AND senha_cliente = %s"
         cursor.execute(query,(email, senha))
         conn.commit()
-        row = cursor.fetchall()
+        row = cursor.fetchone()
     except Exception as e:
         print(f"ERRO: {e}")
-    finally:
-        cursor.close()
-        conn.close()
     
     if row:
-        print("Login realizado com sucesso!")
-        return True
+        print("\nLogin realizado com sucesso!\n")
+        return row
     else:
         print("Usuário ou senha inválido!")
         return False
-
-    
